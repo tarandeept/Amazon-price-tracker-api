3e552fb8a85d0328b7441f06161b57fbb4d6a216
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-08-17 22:02:42 -0700

    Setup basic frontend prototype

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1>This is the home page</h1>
+    <div className="home-container">
+      <div className="row">
+        <h1>Amazon Price Tracker</h1>
+      </div>
+
+      <div className="row searchbar">
+        <form action="/api/v1/subscriptions">
+          <label for="product_url">Enter product URL</label>
+          <input type="text" id="product_url" name="product_url"/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    </div>
   )
 }
 
